a84bd8f0abc5e0f577d5730d609fe76db1b7c81c
Dmitry CHekodanov <sylar00gray@yandex.ru>
2022-06-22 12:45:21 +0300

    Version two

--- head.py
+++ head.py
@@ -1,5 +1,18 @@
-from body import a
+import requests
+from bs4 import BeautifulSoup
 
-b = 77
+url = "https://health-diet.ru/table_calorie/?utm_source=leftMenu&utm_medium=table_calorie"
 
-print(a*b)
+
+headers = {
+    "Accept": "*/*",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36"
+    }
+
+req = requests.get(url, headers=headers)
+
+src = req.text
+# print(src)
+
+with open("index.html", "w") as file:
+     file.write(src)
