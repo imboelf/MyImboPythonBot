ba6541d725176b11c73741fd822465c7f88195b6
Dmitry CHekodanov <sylar00gray@yandex.ru>
2022-06-23 09:41:33 +0300

    Version three

--- head.py
+++ head.py
@@ -1,18 +1,51 @@
 import requests
+import json
 from bs4 import BeautifulSoup
-
-url = "https://health-diet.ru/table_calorie/?utm_source=leftMenu&utm_medium=table_calorie"
-
-
+#
+# url = "https://health-diet.ru/table_calorie/?utm_source=leftMenu&utm_medium=table_calorie"
+#
+#
 headers = {
     "Accept": "*/*",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36"
     }
+#
+# req = requests.get(url, headers=headers)
+#
+# src = req.text
+# ------------------------------create html-file------------------------------
+# with open("index.html", "w", encoding='utf-8') as file:
+#      file.write(src)
 
-req = requests.get(url, headers=headers)
+#------------------------------open html-file and create json------------------------------
+# with open("index.html", encoding="utf-8") as file:
+#     src = file.read()
+#
+# soup = BeautifulSoup(src, "lxml")
+#
+# all_products_hrefs = soup.find_all(class_='mzr-tc-group-item-href')
+#
+# all_categories_dict = {}
+#
+# for item in all_products_hrefs:
+#     item_text = item.text
+#     item_href = "https://health-diet.ru/" + item.get("href")
+#     all_categories_dict[item_text] = item_href
+#
+# with open("all_categories_dict.json", "w", encoding="utf-8") as file:
+#     json.dump(all_categories_dict, file, indent=4, ensure_ascii=False)
+#------------------------------open json------------------------------
 
-src = req.text
-# print(src)
+with open("all_categories_dict.json", encoding="utf-8") as file:
+    all_categories = json.load(file)
+for category_name, category_href in all_categories.items():
 
-with open("index.html", "w") as file:
-     file.write(src)
+    rep = [",", " ", "-", "_"]
+    for item in rep:
+        if item in category_name:
+            category_name = category_name.replace(item, "_")
+    req = requests.get(url=category_href, headers=headers)
+    src = req.text
+
+    with open(f"{category_name}.html", "w", encoding="utf-8") as file:
+        file.write(src)
